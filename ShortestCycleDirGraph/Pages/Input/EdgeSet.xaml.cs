1d2b6d13bb009ac9f75517e1c4ab77fab095480b
Mateusz Tracz <matishadow@gmail.com>
2016-04-26 16:26:54 +0200

    add vital pages

---
+++ ShortestCycleDirGraph/Pages/Input/EdgeSet.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShortestCycleDirGraph.Pages.Input
+{
+    /// <summary>
+    /// Interaction logic for EdgeSet.xaml
+    /// </summary>
+    public partial class EdgeSet : UserControl
+    {
+        public EdgeSet()
+        {
+            InitializeComponent();
+        }
+    }
+}
