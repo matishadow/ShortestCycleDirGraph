fdfa1424c5b8b8edeaf8544b88dff67d134992da
Mateusz Tracz <matishadow@gmail.com>
2016-05-04 15:37:32 +0200

    import done

---
+++ ShortestCycleDirGraph/Core/GrapthImporting.cs
@@ -0,0 +1,84 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ShortestCycleDirGraph.Core
+{
+    public class GrapthImporting
+    {
+        // TODO: generic values
+        // checked
+        public static Graph<int> FromAdjMatrix(sbyte[,] adjMatrix)
+        {
+            var graph = new Graph<int>();
+
+            int size = adjMatrix.GetLength(0);
+
+            for (int i = 0; i < size; i++) { graph.VertexSet.Add(new Vertex<int>(i + 1)); }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (adjMatrix[i, j] == 1) graph.VertexSet[i].Adjacents.Add(graph.VertexSet[j]);
+                }
+            }
+            return graph;
+        }
+
+        //checked
+        public static Graph<int> FromIncMatrix(sbyte[,] incMatrix)
+        {
+            // invalid form of matrix
+            if (!Matrix<int>.checkValidIncMatrix(incMatrix)) incMatrix = Matrix<sbyte>.Transpose(incMatrix);
+
+            var graph = new Graph<int>();
+            for (int i = 0; i < incMatrix.GetLength(0); i++) { graph.VertexSet.Add(new Vertex<int>(i + 1)); }
+
+            for (int i = 0; i < incMatrix.GetLength(1); i++)
+            {
+                for (int j = 0; j < incMatrix.GetLength(0); j++)
+                {
+                    if (incMatrix[j, i] != 1) continue;
+                    for (int k = 0; k < incMatrix.GetLength(0); k++)
+                    {
+                        if (incMatrix[k, i] == -1) graph.VertexSet[k].Adjacents.Add(graph.VertexSet[j]);
+                    }
+                }
+            }
+
+            return graph;
+        }
+
+        //checked
+        public static Graph<int> FromEdgeSet(string edgeSet)
+        {
+            var graph = new Graph<int>();
+
+            var edges = Regex.Replace(edgeSet, " ", "").Split(',');
+
+            foreach (string t1 in edges)
+            {
+                var edge = Regex.Split(t1, "->");
+                foreach (string t in edge)
+                {
+                    var v = new Vertex<int>(int.Parse(t));
+                    if (!graph.VertexSet.Contains(v))
+                        graph.VertexSet.Add(v);
+                }
+            }
+
+            foreach (string t in edges)
+            {
+                var edge = Regex.Split(t, "->");
+
+                Vertex<int> left = null, right = null;
+                foreach (var t1 in graph.VertexSet)
+                {
+                    if (int.Parse(edge[0]) == t1.Value) left = t1;
+                    if (int.Parse(edge[1]) == t1.Value) right = t1;
+                }
+                left?.Adjacents.Add(right);
+            }
+
+            return graph;
+        }
+    }
+}
---
+++ ShortestCycleDirGraph/Core/Matrix.cs
@@ -0,0 +1,39 @@
+﻿namespace ShortestCycleDirGraph.Core
+{
+    public class Matrix<T>
+    {
+        public static T[,] Transpose(T[,] m)
+        {
+            int rows = m.GetLength(0);
+            int columns = m.GetLength(1);
+
+            var transposed = new T[columns,rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    transposed[j, i] = m[i, j];
+                }
+            }
+
+            return transposed;
+        }
+
+        public static bool checkValidIncMatrix(sbyte[,] m)
+        {
+            bool valid = true;
+            bool posOne = false, negOne = false;
+
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                if (m[i, 0] == 1) posOne = !posOne;
+                if (m[i, 0] == -1) negOne = !negOne;
+            }
+
+
+            valid = posOne && negOne;
+            return valid;
+        }
+    }
+}
